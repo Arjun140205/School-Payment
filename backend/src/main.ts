3b0ce5f958440951a4ed39b86f2ab8b325140286
Arjunbir Singh <arjunbirsingh1699@gmail.com>
2025-09-23 19:59:40 +0530

    Update backend main entry file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Add this line
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  // Add global validation pipe
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
